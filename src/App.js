81e512182d22281d3d0fd61a05573eb9189b7801
Mahmoud <mahmoud@Mahmouds-MacBook-Pro.local>
2021-10-17 17:33:35 +0200

    m

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
-import Table from "./components/Table.js";
+import Table from "./views/Table.js";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./components/Header";
-import Stats from "./components/Stats";
+import Stats from "./views/Stats";
 
 function App() {
   return (
